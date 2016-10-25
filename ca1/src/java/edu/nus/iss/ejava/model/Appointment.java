e784f88d5aba87b62717e10a2982e12008ce3fa9
enthirant <enthirant@gmail.com>
2016-10-25 13:52:44 +0800

    CA1- Updated Model of table People and Appointment

--- ca1/src/java/edu/nus/iss/ejava/model/Appointment.java
+++ ca1/src/java/edu/nus/iss/ejava/model/Appointment.java
@@ -6,36 +6,81 @@
 package edu.nus.iss.ejava.model;
 
 import java.io.Serializable;
-import javax.persistence.Basic;
+import java.util.Date;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "appointment")
-@XmlRootElement
-@NamedQueries({
-	@NamedQuery(name = "Appointment.findAppointmentByPeople", query = "SELECT a FROM Appointment a")
-                })
 public class Appointment implements Serializable {
     private static final long serialVersionUID = 1L;
     
-    @Id
-    @Basic(optional = false)
     @NotNull
+    @Id
     @Column(name = "appt_id")
-    private Integer apptId;
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer apptId = 1;
     
+    @NotNull
+    @Column(name = "description")
+    private String description;
+    
+    @Column(name = "appt_date")
+    private Date apptDate;
+    
+    @NotNull
+    @JoinColumn(name="pid")
+    @ManyToOne
+    private People people;
+    
+    public Integer getApptId() {
+        return apptId;
+    }
+
     public void setApptId(Integer apptId) {
         this.apptId = apptId;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getApptDate() {
+        return apptDate;
+    }
+
+    public void setApptDate(Date apptDate) {
+        this.apptDate = apptDate;
+    }
+
+    public People getPeople() {
+        return people;
+    }
+
+    public void setPeople(People people) {
+        this.people = people;
+    }
     
-    public Integer getApptId() {
-        return this.apptId;
+    public JsonObject toJSON() {
+        return (Json.createObjectBuilder()
+                .add("appointmentId", apptId)
+		.add("dateTime", apptDate.getTime())
+		.add("description", description)
+                .add("personId", people.getpId())
+		.build());
     }
 }
--- ca1/src/java/edu/nus/iss/ejava/model/People.java
+++ ca1/src/java/edu/nus/iss/ejava/model/People.java
@@ -6,23 +6,84 @@
 package edu.nus.iss.ejava.model;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.json.JsonObject;
+import javax.json.Json;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "people")
-@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "People.findPeopleByEmail", query = "SELECT p FROM People p WHERE p.email = :email")
+})
 public class People implements Serializable {
     private static final long serialVersionUID = 1L;
     
+    @NotNull
     @Id
     @Basic(optional = false)
-    @NotNull
     @Column(name = "pid")
-    private Integer pId;
+    private String pId;
+    
+    @NotNull
+    @Column(name = "name")
+    @Size(max = 128)
+    private String name;
+           
+    @NotNull
+    @Column(name = "email")
+    @Size(max = 128)
+    private String email;
+
+    @OneToMany(mappedBy = "people")
+    private List<Appointment> lstAppointment;
+
+    public String getpId() {
+        return pId;
+    }
+
+    public void setpId(String pId) {
+        this.pId = pId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Appointment> getLstAppointment() {
+        return lstAppointment;
+    }
+
+    public void setLstAppointment(List<Appointment> lstAppointment) {
+        this.lstAppointment = lstAppointment;
+    }
+    
+    public JsonObject toJSON() {
+        return (Json.createObjectBuilder()
+                .add("pId", pId)
+		.add("name", name)
+		.add("email", email)
+		.build());
+    }
 }
